253b2df275b3bc2eab37a597f107d7f80a843d67
fireb <fireb@ALFREDDABUTTLER>
2024-03-15 20:36:20 -0700

    Added pgn reader

---
+++ ChessBrowser/PgnReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    internal class PgnReader
+    {
+        public ChessGame PgnReader(string filePath)
+        {
+            string pattern = "'([^']*)'";
+            Regex regex = new Regex(pattern);
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    if (line.StartsWith("["))
+                    {
+                        MatchCollection match = regex.Matches(line);
+                    }
+                }
+            }
+
+    }
+
+    }
+}
