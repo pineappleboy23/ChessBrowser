afeb731e0539dbc24523fe0e01fddc6d372c51bd
711tr <711tr@DESKTOP-4E1TR4T>
2024-03-15 19:01:20 -0600

    Add project files.

---
+++ ChessBrowser/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChessBrowser
+{
+    [Register( "AppDelegate" )]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
